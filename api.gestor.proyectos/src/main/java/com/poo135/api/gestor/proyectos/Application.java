4383653eb8cc51c0a5c1cd241d1bece9c690fdb5
Kevin Lemus <nkevinmanuel94@gmail.com>
2025-09-25 20:58:31 -0600

    Initialize new Spring Boot project structure with Maven support
    - Added .vscode/settings.json for Java configuration.
    - Created .gitattributes and .gitignore files for project management.
    - Introduced Maven wrapper files (.mvn/wrapper/maven-wrapper.properties, mvnw, mvnw.cmd) for easier Maven execution.
    - Added pom.xml for project dependencies and configuration.
    - Implemented main application class (Application.java) and basic application properties (application.properties).
    - Created initial test class (ApplicationTests.java) for context loading verification.
    - Removed old project files and configurations from appgestortareas.

---
+++ api.gestor.proyectos/src/main/java/com/poo135/api/gestor/proyectos/Application.java
@@ -0,0 +1,13 @@
+package com.poo135.api.gestor.proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
