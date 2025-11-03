64b06992dbde82a950c4ebd29f30bb549d195586
Kevin Manuel Lemus <51067004+LN13002@users.noreply.github.com>
2025-11-03 15:16:43 -0600

    Agregando Lombok

--- apigestorproyectos/src/main/java/com/equipo7/apigestorproyectos/models/Proyecto.java
+++ apigestorproyectos/src/main/java/com/equipo7/apigestorproyectos/models/Proyecto.java
@@ -2,21 +2,28 @@ package com.equipo7.apigestorproyectos.models;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "proyectos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Proyecto {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
 
-    @Column(length = 1000) // descripción más larga
+    @Column(length = 1000)
     private String descripcion;
 
     private LocalDate fechaInicio;
@@ -28,67 +35,6 @@ public class Proyecto {
 
     private BigDecimal presupuesto;
 
-    // Relacion con tareas (necesaria para JPQL y el diseño relacional)
-    // Un proyecto tiene muchas tareas
-    @OneToMany(mappedBy = "proyecto",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true, // Al quitar una tarea de la lista se elimina de la BD
-            fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "proyecto", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Tarea> tareas = new ArrayList<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public LocalDate getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(LocalDate fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public LocalDate getFechaFinEstimada() {
-        return fechaFinEstimada;
-    }
-
-    public void setFechaFinEstimada(LocalDate fechaFinEstimada) {
-        this.fechaFinEstimada = fechaFinEstimada;
-    }
-
-    public EstadoProyecto getEstado() {
-        return estado;
-    }
-
-    public void setEstado(EstadoProyecto estado) {
-        this.estado = estado;
-    }
-
-    public BigDecimal getPresupuesto() {
-        return presupuesto;
-    }
-
-    public void setPresupuesto(BigDecimal presupuesto) {
-        this.presupuesto = presupuesto;
-    }
 }
