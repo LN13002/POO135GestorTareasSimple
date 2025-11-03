7c2a3f281ef97674204b02d4a31a0e1f210dde61
Kevin Manuel Lemus <51067004+LN13002@users.noreply.github.com>
2025-11-03 15:12:07 -0600

    Se agrega Lombok a la entidad de Empleados.

--- apigestorproyectos/src/main/java/com/equipo7/apigestorproyectos/models/Empleado.java
+++ apigestorproyectos/src/main/java/com/equipo7/apigestorproyectos/models/Empleado.java
@@ -2,16 +2,16 @@ package com.equipo7.apigestorproyectos.models;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "empleados", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Empleado {
 
     @Id
@@ -28,52 +28,4 @@ public class Empleado {
     private LocalDate fechaContratacion;
 
     private Boolean activo;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public LocalDate getFechaContratacion() {
-        return fechaContratacion;
-    }
-
-    public void setFechaContratacion(LocalDate fechaContratacion) {
-        this.fechaContratacion = fechaContratacion;
-    }
-
-    public Boolean getActivo() {
-        return activo;
-    }
-
-    public void setActivo(Boolean activo) {
-        this.activo = activo;
-    }
 }
